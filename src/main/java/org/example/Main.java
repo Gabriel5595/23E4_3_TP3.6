6befc5b668589476fce44561dd939bd62198c3a4
Gabriel5595 <gabriel.vcarvalho@al.infnet.edu.br>
2023-12-05 22:21:39 -0300

    Finalização do Projeto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        URL urlObj = new URL("https://api.agify.io/?name=Gabriel");
+        HttpURLConnection conexao = (HttpURLConnection)urlObj.openConnection();
+        conexao.setRequestMethod("GET");
+        int responseCode = conexao.getResponseCode();
+        if(responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(conexao.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            } in.close();
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            RespostaJSON respostaJSON = objectMapper.readValue(response.toString(), RespostaJSON.class);
+
+            System.out.println(respostaJSON.getName());
+        } else {
+            System.out.println("GET request did not work");
+        }
+    }
+}
